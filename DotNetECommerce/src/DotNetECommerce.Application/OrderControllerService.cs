4ab6fd5466843d9802b0731f83efe7bad7ce7f3b
igayamaguchi <iganin2@gmail.com>
2020-04-11 18:20:26 +0900

    wip: order

---
+++ DotNetECommerce/src/DotNetECommerce.Application/OrderControllerService.cs
@@ -0,0 +1,64 @@
+﻿using DotNetECommerce.Domain.Members;
+using DotNetECommerce.Domain.Orders;
+using DotNetECommerce.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetECommerce.Application
+{
+    public class OrderControllerService
+    {
+        private readonly IMemberRepository memberRepository;
+        private readonly IProductRepository productRepository;
+
+        public OrderControllerService(IMemberRepository memberRepository, IProductRepository productRepository)
+        {
+            this.memberRepository = memberRepository;
+            this.productRepository = productRepository;
+        }
+
+        public OrderResult Order(Guid productId)
+        {
+            // TODO: WIP
+            var product = productRepository.Find(productId);
+
+            if (product == null)
+            {
+                return new OrderResult { ResultType = OrderResultType.NotFoundProduct };
+            }
+
+
+            return new OrderResult { ResultType = OrderResultType.Success, Order = null };
+        }
+
+        public void Order(Guid memberId, Guid productId)
+        {
+            // TODO: WIP
+            var member = memberRepository.Find(memberId);
+            if (member == null)
+            {
+
+            }
+
+            if (member.State != MemberState.Registered)
+            {
+
+            }
+
+        }
+    }
+
+    public enum OrderResultType
+    {
+        Success,
+        NotFoundMmeber,
+        NotFoundProduct,
+    }
+
+    public class OrderResult
+    {
+        public OrderResultType ResultType { get; set; }
+        public Order Order { get; set; }
+    }
+}
--- DotNetECommerce/src/DotNetECommerce.Domain/Orders/Order.cs
+++ DotNetECommerce/src/DotNetECommerce.Domain/Orders/Order.cs
@@ -6,5 +6,33 @@ namespace DotNetECommerce.Domain.Orders
 {
     public class Order : IAggregateRoot
     {
+        /**
+         * 商品購入時には料金を支払い、発送者、発送先住所を入力する。
+         * 注文には会員登録は不要。
+         * 会員登録しているとポイントを受け取ることが出来る。
+         * 会員登録をせず注文をする場合はメールアドレスが必要。
+         */
+
+        public Guid OrderId { get; private set; }
+
+        public DateTime OrderedOn { get; private set; }
+
+        public Guid ProductId { get; private set; }
+
+        public Guid MemberId { get; private set; }
+
+        public string MailAddress { get; private set; }
+
+        protected Order()
+        {
+            OrderId = Guid.NewGuid();
+            OrderedOn = DateTime.Now;
+        }
+
+        public Order Create(Guid productId)
+        {
+            // TODO: WIP
+            return null;
+        }
     }
 }
